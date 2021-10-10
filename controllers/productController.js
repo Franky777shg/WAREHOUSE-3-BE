b5dfbd33fce1082f295c4d1de8cfe812f50f5a5c
manridho <usman.manridho@gmail.com>
2021-10-11 05:46:17 +0700

    As a user, I want to be able to sort the products list by its name or price

--- controllers/productController.js
+++ controllers/productController.js
@@ -37,7 +37,7 @@ module.exports = {
 
             let totalNameCate
             console.log("FilterNamaAndKategori")
-            
+
             let countNameCate = `select count(*) as totalItems 
             from product p1 inner join categories c2 
             on p1.id_categories = c2.id_categories 
@@ -49,23 +49,51 @@ module.exports = {
                     res.status(400).send(errCountNameCate)
                 }
                 totalNameCate = resCountNameCate[0]
-                
+
                 let getProdCateName = `select * from product p1 inner join categories c2 
                 on p1.id_categories = c2.id_categories 
                 where product_name like '%${name}%' and category_name like '%${category}%' 
                 limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
-                
-                db.query(getProdCateName, (errGetProdCateName, resGetProdNameCate) =>{
-                    if(errGetProdCateName){
+
+                db.query(getProdCateName, (errGetProdCateName, resGetProdNameCate) => {
+                    if (errGetProdCateName) {
                         res.status(400).send(errGetProdCateName)
                     }
                     let resProdNameCate = []
-                    resProdNameCate.push(resGetProdNameCate, { current_page: currentPage},{per_page: perPage}, totalNameCate)
+                    resProdNameCate.push(resGetProdNameCate, { current_page: currentPage }, { per_page: perPage }, totalNameCate)
                     res.status(200).send(resProdNameCate)
                 })
-            })   
+            })
 
-        } else if(!name) { // filter dengan kategori saja
+        } else if (!name && !category) { // filter tanpa inputan
+            let totalCommon
+            console.log("filterCommon")
+
+            let countCommon = `select count(*) as totalItems 
+            from product p1 inner join categories c2 
+            on p1.id_categories = c2.id_categories ;`
+
+            db.query(countCommon, (errCountCommon, resCountCommon) => {
+                if (errCountCommon) {
+                    res.status(400).send(errCountCommon)
+                }
+                totalCommon = resCountCommon[0]
+
+                let getProdCommon = `select * from product p1 inner join categories c2 
+                on p1.id_categories = c2.id_categories
+                limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
+
+                db.query(getProdCommon, (errGetProdCommon, resGetProdCommon) => {
+                    if (errGetProdCommon) {
+                        res.status(400).send(errGetProdCommon)
+                    }
+                    let resProdCommon = []
+                    resProdCommon.push(resGetProdCommon, { current_page: currentPage }, { per_page: perPage }, totalCommon)
+                    res.status(200).send(resProdCommon)
+                })
+            })
+
+        } else if (!name ) { //filter dengan kategori
 
             let totalCate
             console.log("FilterKategori")
@@ -76,7 +104,7 @@ module.exports = {
             where category_name like '%${category}%';`
 
             db.query(countCate, (errCountCate, resCountCate) => {
-                if(errCountCate) {
+                if (errCountCate) {
                     res.status(400).send(errCountCate)
                 }
                 totalCate = resCountCate[0]
@@ -87,46 +115,17 @@ module.exports = {
                 limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
 
                 db.query(getProdCate, (errGetProdCate, resGetProdCate) => {
-                    if(errGetProdCate) {
+                    if (errGetProdCate) {
                         res.status(400).send(resGetProdCate)
                     }
                     let resProdCate = []
-                    resProdCate.push(resGetProdCate, {current_page: currentPage},{per_page: perPage}, totalCate)
+                    resProdCate.push(resGetProdCate, { current_page: currentPage }, { per_page: perPage }, totalCate)
                     res.status(200).send(resProdCate)
                 })
             })
 
-        } else if(!name && !category) { //tidak ada inputan
-            
-            let totalCommon
-            console.log("filterCommon")
+        } else if (!category) { //filter dengan nama saja
 
-            let countCommon = `select count(*) as totalItems 
-            from product p1 inner join categories c2 
-            on p1.id_categories = c2.id_categories ;`
-
-            db.query(countCommon,(errCountCommon, resCountCommon) => {
-                if(errCountCommon) {
-                    res.status(400).send(errCountCommon)
-                }
-                totalCommon = resCountCommon[0]
-
-                let getProdCommon = `select * from product p1 inner join categories c2 
-                on p1.id_categories = c2.id_categories
-                limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
-
-                db.query(getProdCommon, (errGetProdCommon, resGetProdCommon) => {
-                    if(errGetProdCommon) {
-                        res.status(400).send(errGetProdCommon)
-                    }
-                    let resProdCommon = []
-                    resProdCommon.push(resGetProdCommon, {current_page: currentPage}, {per_page: perPage}, totalCommon)
-                    res.status(200).send(resProdCommon)
-                })
-            })
-
-        } else if(!category) { //filter dengan nama saja
-            
             let totalName
             console.log("filterName")
 
@@ -136,7 +135,7 @@ module.exports = {
             where product_name like '%${name}%';`
 
             db.query(countName, (errCountName, resCountName) => {
-                if(errCountName) {
+                if (errCountName) {
                     res.status(400).send(errCountName)
                 }
                 totalName = resCountName[0]
@@ -147,11 +146,11 @@ module.exports = {
                 limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
 
                 db.query(getProdName, (errGetProdName, resGetProdName) => {
-                    if(errGetProdName) {
+                    if (errGetProdName) {
                         res.status(400).send(errGetProdName)
                     }
                     let resProdName = []
-                    resProdName.push(resGetProdName, {current_page: currentPage},{per_page: perPage}, totalName)
+                    resProdName.push(resGetProdName, { current_page: currentPage }, { per_page: perPage }, totalName)
                     res.status(200).send(resProdName)
                 })
             })
@@ -159,5 +158,139 @@ module.exports = {
 
     },
 
+    sortingProduct: (req, res) => {
+        const { name, category, orderBy, sortBy } = req.body
+        const currentPage = parseInt(req.query.page) || 1 //default query page
+        const perPage = parseInt(req.query.perPage) || 8 //default query per-page
+
+        //Jika filter nama & kategori
+        if (category && name) {
+
+            let totalNameCate
+            console.log("FilterNamaAndKategori")
+
+            let countNameCate = `select count(*) as totalItems 
+            from product p1 inner join categories c2 
+            on p1.id_categories = c2.id_categories 
+            where product_name like '%${name}%' and category_name like '%${category}%';`
+            // console.log("countNameCate",countNameCate)
+
+            db.query(countNameCate, (errCountNameCate, resCountNameCate) => {
+                if (errCountNameCate) {
+                    res.status(400).send(errCountNameCate)
+                }
+                totalNameCate = resCountNameCate[0]
+
+                let getProdCateName = `select * from product p1 inner join categories c2 
+                on p1.id_categories = c2.id_categories 
+                where product_name like '%${name}%' and category_name like '%${category}%'
+                order by ${orderBy} ${sortBy}  
+                limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
+
+                db.query(getProdCateName, (errGetProdCateName, resGetProdNameCate) => {
+                    if (errGetProdCateName) {
+                        res.status(400).send(errGetProdCateName)
+                    }
+                    let resProdNameCate = []
+                    resProdNameCate.push(resGetProdNameCate, { current_page: currentPage }, { per_page: perPage }, totalNameCate)
+                    res.status(200).send(resProdNameCate)
+                })
+            })
+
+        } else if (!name && !category) { // filter tanpa input
+
+            let totalCommon
+            console.log("filterCommon")
+
+            let countCommon = `select count(*) as totalItems 
+            from product p1 inner join categories c2 
+            on p1.id_categories = c2.id_categories ;`
+
+            db.query(countCommon, (errCountCommon, resCountCommon) => {
+                if (errCountCommon) {
+                    res.status(400).send(errCountCommon)
+                }
+                totalCommon = resCountCommon[0]
+
+                let getProdCommon = `select * from product p1 inner join categories c2 
+                on p1.id_categories = c2.id_categories
+                order by ${orderBy} ${sortBy}
+                limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
+
+                db.query(getProdCommon, (errGetProdCommon, resGetProdCommon) => {
+                    if (errGetProdCommon) {
+                        res.status(400).send(errGetProdCommon)
+                    }
+                    let resProdCommon = []
+                    resProdCommon.push(resGetProdCommon, { current_page: currentPage }, { per_page: perPage }, totalCommon)
+                    res.status(200).send(resProdCommon)
+                })
+            })
+
+        } else if (!name) { //filter dengan kategori
+            let totalCate
+            console.log("FilterKategori")
+
+            let countCate = `select count(*) as totalItems 
+            from product p1 inner join categories c2 
+            on p1.id_categories = c2.id_categories 
+            where category_name like '%${category}%';`
+
+            db.query(countCate, (errCountCate, resCountCate) => {
+                if (errCountCate) {
+                    res.status(400).send(errCountCate)
+                }
+                totalCate = resCountCate[0]
+
+                let getProdCate = `select * from product p1 inner join categories c2 
+                on p1.id_categories = c2.id_categories 
+                where category_name like '%${category}%'
+                order by ${orderBy} ${sortBy} 
+                limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
+
+                db.query(getProdCate, (errGetProdCate, resGetProdCate) => {
+                    if (errGetProdCate) {
+                        res.status(400).send(resGetProdCate)
+                    }
+                    let resProdCate = []
+                    resProdCate.push(resGetProdCate, { current_page: currentPage }, { per_page: perPage }, totalCate)
+                    res.status(200).send(resProdCate)
+                })
+            })
+
+
+        } else if (!category) { //filter dengan nama saja
+
+            let totalName
+            console.log("filterName")
+
+            let countName = `select count(*) as totalItems 
+            from product p1 inner join categories c2 
+            on p1.id_categories = c2.id_categories 
+            where product_name like '%${name}%';`
+
+            db.query(countName, (errCountName, resCountName) => {
+                if (errCountName) {
+                    res.status(400).send(errCountName)
+                }
+                totalName = resCountName[0]
+
+                let getProdName = `select * from product p1 inner join categories c2 
+                on p1.id_categories = c2.id_categories 
+                where product_name like '%${name}%'
+                order by ${orderBy} ${sortBy}
+                limit ${db.escape((currentPage - 1) * perPage)}, ${db.escape(perPage)};`
+
+                db.query(getProdName, (errGetProdName, resGetProdName) => {
+                    if (errGetProdName) {
+                        res.status(400).send(errGetProdName)
+                    }
+                    let resProdName = []
+                    resProdName.push(resGetProdName, { current_page: currentPage }, { per_page: perPage }, totalName)
+                    res.status(200).send(resProdName)
+                })
+            })
+        }
+    }
 }
 
